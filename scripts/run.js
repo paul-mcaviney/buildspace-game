f684c82fa97f227d90ef9b3e9c326190b93e0905
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-10-23 12:59:36 -0600

    Initial commit

---
+++ scripts/run.js
@@ -0,0 +1,27 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory("BlockchainGame");
+    const gameContract = await gameContractFactory.deploy(
+        ["Goku", "Android 18", "Gohan"],    // Names
+        ["https://i.imgur.com/E6uySNo.jpg", // Images - Goku
+        "https://i.imgur.com/SWciGrF.jpg",  // Images - Android 18
+        "https://i.imgur.com/hybQozx.jpg"   // Images - Gohan
+        // Images - Cell - https://i.imgur.com/e7eMUwQ.jpg
+    ],
+    [1000, 600, 750],                       // Hp Values
+    [1000, 1400, 1250],                     // Attack Damage
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
